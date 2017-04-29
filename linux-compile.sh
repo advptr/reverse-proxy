da6b31633bbe0c5d63e38e38a6ad5ff38665bdf8
Peter Larsson <peter.larsson@callistaenterprise.se>
2017-04-29 12:57:45 +0200

    use output file name

--- linux-compile.sh
+++ linux-compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-name=$1
-docker run --rm -it -v "$GOPATH":/gopath -v "$(pwd)":/app -e "GOPATH=/gopath" -w /app golang:1.8 sh -c 'CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o app.out'
-[ "$name" != "" ] && mv app.out $1
+# requires docker, and performs a native linux compilation of a go program (uses golang 1.8 container)
+# default output file is app.out and might overidden by a program argument
+name=${1:-app.out}
+docker run --rm -it -v "$GOPATH":/gopath -v "$(pwd)":/app -e "GOPATH=/gopath" -w /app golang:1.8 sh -c "CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags=\"-s\" -o ${name}"
